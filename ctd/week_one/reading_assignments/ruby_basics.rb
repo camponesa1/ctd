acaf3723a5327d64e05681620c2c55b48d069712
Katia <kat_camp@hotmail.com>
2018-09-20 23:23:27 +0000

    initial commit

---
+++ ctd/week_one/reading_assignments/ruby_basics.rb
@@ -0,0 +1,61 @@
+#assigment ruby basics - first class  week one
+
+# output an integer
+
+puts (365/2)+1
+puts "is the number of the days of a half year"
+
+puts "how many seconds are in a year?"
+puts 365*24*60*60
+puts
+puts "how many minutes are in a decade?"
+puts 10*365*24*60
+puts
+puts "how many seconds are you?"
+puts "if you have 40 years old then you have "
+puts 40*365*24*60*60
+puts "seconds of life"
+puts "oh " * 4
+
+
+# output the number of seconds in a week
+
+puts "how many seconds are in a week?"
+puts 7*24*60*60
+puts "oh " * 3
+
+# output a float
+puts "Pi value is"
+puts 3.1415
+puts "Pi it is a famous float number"
+
+# output a string
+puts "Hello "
+print "What is your first name?"
+name=gets.chomp
+puts "Wow #{name} is a beautiful name!"
+
+# output concatination of strings
+
+print "What is your middle name?"
+midname=gets.chomp
+puts "Ok. And what is your last name?"
+lastname=gets.chomp
+puts "Please to meet you #{name} #{midname} #{lastname}"
+
+print "which is your favorite number?"
+
+favnumber=gets. chomp
+
+puts "what about"
+
+bestnumber= favnumber +1
+
+puts bestnumber "It is a better number!"
+
+
+
+#what I tried:
+#puts bestnumber=favnumber +1
+#puts "what about" {favnumber to_i +1} ? "It is a better number!"
+#puts "what about #{bestnumber}? It is a better number!"
